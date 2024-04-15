b1b4dc4031612e42dbac2fc1533d0e8edae97695
simon <simonzhang0428@gmail.com>
2024-04-14 21:00:12 -0700

    Exercise 1: Practice with Files and String Streams

--- proj2/command.cpp
+++ proj2/command.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 // Function Prototypes
 
-void InterpretCommands(istream&);
+void InterpretCommands(istream &);
 
-void InterpretUpdate(istream&);
+void InterpretUpdate(istream &);
 
-void InterpretList(istream&);
+void InterpretList(istream &);
 
-void InterpretBatch(istream&);
+void InterpretBatch(istream &);
 
-void InterpretQuit(istream&);
+void InterpretQuit(istream &);
 
 /**
  * Driver Function
@@ -26,20 +26,20 @@ int main()
 }
 
 /**
- * TODO: Write your function description here.
+ * Interpret and Validate commands including update, list, batch, quit
  */
-void InterpretCommands(istream& cmds)
+void InterpretCommands(istream &cmds)
 {
     string line, lineInWord;
     // TODO: Declare other variables here.
 
-    while (________) // TODO: Get a line.
+    while (getline(cmds, line)) // TODO: Get a line.
     {
-        istringstream lineIn(line);
+        istringstream lineIn(line.c_str());
 
-        if (________) // TODO: Get the line's first word.
+        if (!(lineIn >> lineInWord)) // TODO: Get the line's first word.
         {
-            ________; // TODO: Do something if there weren't any words.
+            cerr << "No word in the input command" << endl; // TODO: Do something if there weren't any words.
         }
         else if (lineInWord == "update")
         {
@@ -62,4 +62,122 @@ void InterpretCommands(istream& cmds)
             cerr << "Unrecognizable command word." << endl;
         }
     }
+}
+
+/*
+ * Update the word with number.
+ * The update command must be followed by a word—a sequence of nonblanks—
+ * and an integer, with no other nonblank characters on the line.
+ * Example: update word number
+*/
+void InterpretUpdate(istream &input)
+{
+    cout << "-------"
+         << "Update"
+         << "-------" << endl;
+    string word;
+    int number;
+    if (input >> word >> number)
+    {
+        cout << "word: " << word << "\tnumber: " << number << endl;
+    }
+    else
+    {
+        cerr << "Invalid input arguments." << endl;
+    }
+
+    string temp;
+    if (input >> temp)
+    {
+        cerr << "Too many arguments." << endl;
+    }
+    return;
+}
+
+/*
+ * List names and quantities.
+ * The list command must be followed by either the word names or the word quantities.
+ * Example: list names / list quantities
+*/
+void InterpretList(istream &input)
+{
+    cout << "-------"
+         << "List"
+         << "-------" << endl;
+    string word, temp;
+    if (input >> word)
+    {
+        if (word == "names" || word == "quantities")
+        {
+            if (input >> temp)
+            {
+                cerr << "Too many arguments." << endl;
+            }
+            else
+            {
+                cout << "Successful" << endl;
+            }
+        }
+        else
+        {
+            cerr << "Arguments must be \"names\" or \"quantities\"" << endl;
+        }
+    }
+    else
+    {
+        cerr << "Invalid input arguments." << endl;
+    }
+    return;
+}
+
+/**
+ * Read, Recognize, and Process commands from the specified file, 
+ * then (assuming that a quit command has not been read) resume reading commands from cin. 
+ * Assume that the file does not contain any batch commands.
+ * The batch command must be followed by the name of an existing file.
+ * Example: batch file-name
+ */
+void InterpretBatch(istream &input)
+{
+    cout << "-------"
+         << "Batch"
+         << "-------" << endl;
+
+    string filename;
+    if (!(input >> filename))
+    {
+        cerr << "Not found file name" << endl;
+        return;
+    }
+    else
+    {
+        ifstream file_stream;
+        file_stream.open(filename.c_str());
+        if (file_stream.fail())
+        {
+            cout << "could not open file " << filename << endl;
+        }
+        else
+        {
+            InterpretCommands(file_stream);
+        }
+    }
+}
+
+/**
+ * Terminate the program. 
+ * The quit command must appear by itself on the line.
+ * Example: quit
+ */
+void InterpretQuit(istream &input)
+{
+    cout << "-------"
+         << "Quit"
+         << "-------" << endl;
+    string temp;
+    if (input >> temp)
+    {
+        cerr << "Too many arguments." << endl;
+    }
+    exit(1);
 }
